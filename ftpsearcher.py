a1f2d24aff914612cc4e21ee9037ec3710050d22
Sunl1ght-Rim <Kirillll.2001@yandex.ru>
2020-12-05 06:46:50 +0300

    Update

--- ftpsearcher.py
+++ ftpsearcher.py
@@ -11,16 +11,16 @@ from colorama import Fore, Style, init
 def get_args():
     print("ver. 1.0\n")
     parser = argparse.ArgumentParser(epilog="rest in pantene")
-    parser.add_argument("-l", type=str, dest="list", default="FTP_list.txt",
-            help="Path to the txt-file with the target FTP list (FTP_list.txt by default). [as default target]")
-    parser.add_argument("-f", type=str, dest="addrs", default=False, nargs="+",
-            help="Single or some target FTP.")
+    parser.add_argument("-l", type=str, dest="list", default="ftplist.txt",
+            help="Path to txt file with FTP target list in form 'host:port' (ftplist.txt by default). [as default target]")
+    parser.add_argument("-f", type=str, dest="hosts", default=False, nargs="+",
+            help="Single or some target FTP with the input data as 'host:port'.")
     parser.add_argument("-ip", dest="range", type=str, default=False,
-            help="Target IP range.")
+            help="Target IP range as 'start-end:port'.")
     parser.add_argument("-r", dest="result", type=str, default="results.html",
             help="Path to html-file for saving results (results.html by default). To canceling writing to the file, enter '-r N'.")
-    parser.add_argument("-q", "--query", dest="obj", type=str, default="",
-            help="Query to search substring in filenames.")
+    parser.add_argument("-q", dest="obj", type=str, default="", nargs="+",
+            help="Queries to search substrings into filenames.")
     parser.add_argument("-A", "--all", dest="obj", action="store_const", const="", default="",
             help="Search all files. [as default query]")
     parser.add_argument("-i", "--images", dest="extns", action="store_const", const=('.jpg', '.jpeg', '.png', '.gif', '.svg', '.bmp', '.tif', '.tiff', '.psd', '.xcf'), default=False,
@@ -33,6 +33,8 @@ def get_args():
             help="Search only by docs (doc, docx, pdf, epub, fb2).")
     parser.add_argument("-z", "--zip", dest="extns", action="store_const", const=('.zip', '.rar', '.7z', '.tar', '.tar.gz', '.cab'), default=False,
             help="Search only by zip or other archive formats (zip, rar, 7z, tar, tar.gz, cab).")
+    parser.add_argument("-s", "--sync", dest="sync", action="store_true", default=False,
+            help="Use only synchronous requests.")
     return parser.parse_args()
 
 # searching for a file by filetype with a param or by a substring of an object with a query.
@@ -43,7 +45,10 @@ def searching(name):
     if args.extns != False:
          [results(full_path, syncnumber) for extension in args.extns if full_path.endswith(extension)]
     else:
-        results(full_path, syncnumber) if args.obj in full_path.split("/")[-1] else None
+        if args.obj == '':
+            results(full_path, syncnumber)
+        else:
+            [results(full_path, syncnumber) for object in args.obj if object in full_path.split("/")[-1]]
 
 # ----------------------------------SYNCHRONOUS BLOCK-------------------------------------------------------------------
 # recursively checking all directories that are not files using the MLSD-method, if it can be used.
@@ -83,13 +88,15 @@ def badftp_cycle(maybe_folder):
         pathlist.pop() if len(pathlist) > 1 else None
 
 # connecting to FTP with ftplib.
-def connect(host):
+def connect(host, cnct_port):
     print(Fore.GREEN + "Now it's " + Fore.WHITE + host + Fore.GREEN + " with MLSD.")
     global pathlist
-    pathlist = [host]
+    host_port = host + ":" + str(cnct_port)
+    pathlist = [host_port]
     global ftp
     try:
         ftp = FTP(host, timeout=7)
+        ftp.connect(port=cnct_port)
         ftp.login()
         global syncnumber
         syncnumber = 0
@@ -119,9 +126,9 @@ def connect(host):
 # aioftp connecting and checking.
 async def asyncgetting(host, port, command, asyncnumber):
     try:
-        client = aioftp.Client(socket_timeout=4)
+        client = aioftp.Client()
         async with async_timeout.timeout(4):
-            await client.connect(host)
+            await client.connect(host, port)
             await client.login()
         print(host + Fore.GREEN + " started with asynchronous method: " + Fore.WHITE + command)
         try:
@@ -131,44 +138,48 @@ async def asyncgetting(host, port, command, asyncnumber):
                         for extension in args.extns:
                             if str(path).endswith(extension):
                                 asyncnumber += 1
-                                full_path = str(host) + "/" + str(path)
+                                full_path = str(host) + ":" + str(port) + "/" + str(path)
                                 results(full_path, asyncnumber)
                     else:
-                        if args.obj in str(path).split("/")[-1]:
+                        if args.obj == '':
                             asyncnumber += 1
-                            full_path = str(host) + "/" + str(path)
+                            full_path = str(host) + ":" + str(port) + "/" + str(path)
                             results(full_path, asyncnumber)
+                        for object in args.obj:
+                            if object in str(path).split("/")[-1]:
+                                asyncnumber += 1
+                                full_path = str(host) + ":" + str(port) + "/" + str(path)
+                                results(full_path, asyncnumber)
             print(host + Fore.GREEN + " was getted.")
 
 # exception handling.
         except aioftp.StatusCodeError as iner:
             if str(iner.received_codes) == "('500',)":
                 if str(iner.info) == "[' Unknown command.']":
-                    print(Fore.GREEN + "MLSD is not supported on server " + Fore.WHITE + host + Fore.GREEN + " Trying to use asynchronous LIST.")
-                    await asyncgetting(host, 21, 'LIST', 0)
+                    print(Fore.GREEN + "MLSD is not supported on server " + Fore.WHITE + host + ":" + str(port) + Fore.GREEN + " Trying to use asynchronous LIST.")
+                    await asyncgetting(host, port, 'LIST', 0)
                 elif "ommand not underst" in str(iner.info):
-                    print(Fore.GREEN + "Asynchronous methods is not available on server " + Fore.WHITE + host + Fore.GREEN + " Trying to use synchronous MLSD.")
-                    connect(host)
+                    print(Fore.GREEN + "Asynchronous methods is not available on server " + Fore.WHITE + host + ":" + str(port) + Fore.GREEN + " Trying to use synchronous MLSD.")
+                    connect(host, port)
                 else:
                     print(iner)
             elif str(iner.received_codes) == "('550',)":
-                print(Fore.GREEN + "Error 550 (Can't check for file existence) with server " + Fore.WHITE + host + Fore.GREEN + ". Trying to use synchronous MLSD.")
-                connect(host)
+                print(Fore.GREEN + "Error 550 (Can't check for file existence) with server " + Fore.WHITE + host + ":" + str(port) + Fore.GREEN + ". Trying to use synchronous MLSD.")
+                connect(host, port)
             else:
                 print(iner)
     except aioftp.StatusCodeError as exer:
-        print(str(exer))
         if str(exer.received_codes) == "('530',)":
-            print(Fore.GREEN + "Login authentication failed onto " + Fore.WHITE + host)
+            print(Fore.GREEN + "Login authentication failed onto " + Fore.WHITE + host + ":" + str(port))
         else:
             print(exer)
     except gaierror:
-        print(host + Fore.RED + " not responding.")
+        print(host + ":" + str(port) + Fore.RED + " not responding.")
     except OSError as err:
         if str(err) == "timed out":
-            print(host + Fore.RED + " not responding.")
+            print(host + ":" + str(port) + Fore.RED + " not responding.")
     except asyncio.TimeoutError:
-        print(host + Fore.RED + " not responding.")
+        print(host + ":" + str(port) + Fore.RED + " not responding.")
     except KeyboardInterrupt:
         print(Fore.RED + "\nYou have interrupted host scanning. Move to the next one.")
 # ----------------------------------------------------------------------------------------------------------------------
@@ -178,9 +189,18 @@ def unpack_list(list):
     try:
         flist = open(list, "r")
         liststr = flist.read().split('\n')
-        for string in liststr:
-            if string != "":
-                tasks.append(asyncgetting(string, 21, "MLSD", 0))
+        if args.sync == True:
+            for string in liststr:
+                if string != "":
+                    host, *port = string.split(":")
+                    port = ([int(port) for port in port]) if port != [] else [21]
+                    connect(host, *port)
+        else:
+            for string in liststr: # repetition to decrease O(n).
+                if string != "":
+                    host, *port = string.split(":")
+                    port = ([int(port) for port in port]) if port != [] else [21]
+                    tasks.append(asyncgetting(host, *port, "MLSD", 0))
         flist.close()
     except FileNotFoundError:
         print(Fore.RED + "File " + Fore.WHITE + list + Fore.RED + " not found.")
@@ -188,20 +208,22 @@ def unpack_list(list):
         print(Fore.RED + "Unable to open results file, but file " + Fore.WHITE + list + Fore.RED + " was found. Check your privileges.")
 
 # opening FTP from ip-range.
-def unpack_range(iprange):
+def unpack_range(range_port):
     import ipaddress
     import struct
+    iprange, *port = range_port.split(':')
+    port = ([int(port) for port in port]) if port != [] else [21]
     try:
         start, end = iprange.split('-')
         start = struct.unpack('>I', socket.inet_aton(start))[0]
         end = struct.unpack('>I', socket.inet_aton(end))[0]
-        addrs = [socket.inet_ntoa(struct.pack('>I', i)) for i in range(start, end)]
-        for addr in addrs:
-            try:
-                tasks.append(asyncgetting(addr, 21, "MLSD", 0))
-            except KeyboardInterrupt:
-                print(Fore.RED + "\nYou have interrupted on the " + Fore.WHITE + addr + Fore.RED + " server. Move on the next.")
-                continue
+        hosts = [socket.inet_ntoa(struct.pack('>I', i)) for i in range(start, end)]
+        if args.sync == True:
+            for host in hosts:
+                connect(host, *port)
+        else:
+            for host in hosts:
+                tasks.append(asyncgetting(host, *port, "MLSD", 0))
     except ValueError:
         print(Fore.RED + "IP-range must be separeted by '-' character.")
 
@@ -238,29 +260,36 @@ def main():
             print(Fore.RED + "File" + Fore.WHITE + args.result + Fore.RED + " not found.")
         except PermissionError:
             print(Fore.RED + "Unable to open results file, but file " + Fore.WHITE +  args.result + Fore.RED + " was found. Check your privileges.")
-    if args.addrs != False:
-        args.range = False
+    if args.hosts != False: # for single or some target FTP.
         args.list = False
         print(Style.DIM + str(args)[10:-1] + "\n")
-        [tasks.append(asyncgetting(addr, 21, "MLSD", 0)) for addr in args.addrs] # for single or some target FTP.
-    elif args.range != False:
-        args.addrs = False
+        if args.sync == True:
+            for host_port in args.hosts:
+                host, *port = host_port.split(":")
+                port = ([int(port) for port in port]) if port != [] else [21]
+                connect(host, *port)
+        else:
+            for host_port in args.hosts: # repetition to decrease O(n).
+                host, *port = host_port.split(":")
+                port = ([int(port) for port in port]) if port != [] else [21]
+                tasks.append(asyncgetting(host, *port, "MLSD", 0))
+    elif args.range != False: # for IP-range.
         args.list = False
         print(Style.DIM + str(args)[10:-1] + "\n")
-        unpack_range(args.range) # for IP-range.
-    else:
+        unpack_range(args.range)
+    else: # for the list in file.
         print(Style.DIM + str(args)[10:-1] + "\n")
-        unpack_list(args.list) # for the list in file.
+        unpack_list(args.list)
+    if args.sync == False:
+        ioloop = asyncio.get_event_loop()
 
-    ioloop = asyncio.get_event_loop()
+        try:
+            ioloop.run_until_complete(asyncio.gather(*tasks))
+            print(Fore.YELLOW + Style.BRIGHT + "Connections completed.")
+        except KeyboardInterrupt:
+            print(Fore.RED + "\nYou have interrupted the FTP Searcher.")
 
-    try:
-        ioloop.run_until_complete(asyncio.gather(*tasks))
-        print(Fore.YELLOW + Style.BRIGHT + "Connections completed.")
-    except KeyboardInterrupt:
-        print(Fore.RED + "\nYou have interrupted the FTP Searcher.")
-
-    ioloop.close()
+        ioloop.close()
 
 if __name__ == '__main__':
     main()
